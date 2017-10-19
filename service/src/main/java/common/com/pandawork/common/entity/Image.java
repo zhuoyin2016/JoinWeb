6152c89df820cf50c44ee5c13d759c8138a2e236
刘喆 <734576823@qq.com>
2017-10-19 18:20:08 +0800

    刘喆

---
+++ service/src/main/java/common/com/pandawork/common/entity/Image.java
@@ -0,0 +1,77 @@
+package com.pandawork.common.entity;
+
+import com.pandawork.core.common.entity.AbstractEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+/**
+ * images实体
+ * Created by houst，liuz on 2016/3/27.
+ *
+ */
+@Table(name = "t_images")
+@Entity
+public class Image extends AbstractEntity {
+    //图片ID
+    @Id
+    public Integer id;
+
+    //图片名称
+    @Column(name = "img_name")
+    private String  imgName;
+
+    //图片上传时间
+    @Column(name = "upload_img_time")
+    private String uploadImgTime;
+
+    @Column(name = "select")
+    //图片是否被选中
+    private Integer select;
+
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getImgName() {
+        return imgName;
+    }
+
+    public void setImgName(String imgName) {
+        this.imgName = imgName;
+    }
+
+    public String getUploadImgTime() {
+        return uploadImgTime;
+    }
+
+    public void setUploadImgTime(String uploadImgTime) {
+        this.uploadImgTime = uploadImgTime;
+    }
+
+    public Integer getSelect() {
+        return select;
+    }
+
+    public void setSelect(Integer select) {
+        this.select = select;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "id=" + id +
+                ", imgName='" + imgName + '\'' +
+                ", uploadImgTime='" + uploadImgTime + '\'' +
+                ", select=" + select +
+                '}';
+    }
+}
