755256f2f0fcef9e5eec4d8f5b4e3063c483ce2a
刘喆 <734576823@qq.com>
2017-10-19 18:22:04 +0800

    刘喆

---
+++ service/src/main/java/mapper/com/pandawork/mapper/ImageMapper.java
@@ -0,0 +1,52 @@
+package com.pandawork.mapper;
+
+import com.pandawork.common.entity.Image;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 轮播图片管理mapper层
+ * Created by houst,liuz on 2016/3/28.
+ *增加图片，删除图片，修改图片，根据id查询图片，查询图片列表
+ *
+ */
+public interface ImageMapper {
+
+    /**
+     * 增加图片
+     * @param image 图片
+     * @throws Exception 异常
+     */
+    public void addImage(@Param("image") Image image) throws Exception;
+
+    /**
+     *根据id删除图片
+     * @param id id
+     * @throws Exception 异常
+     */
+    public boolean delImageById(@Param("id") int id) throws Exception;
+
+    /**
+     * 修改图片
+     * @param image 图片
+     * @throws Exception 异常
+     */
+    public void updateImage(@Param("image") Image image) throws Exception;
+
+    /**
+     * 根据id查找图片
+     * @param id id
+     * @return 返回
+     * @throws Exception 异常
+     */
+    public  Image queryImageById(@Param("id") int id) throws Exception;
+
+    /**
+     * 查询所有图片列表
+     * @return 返回
+     * @throws Exception 异常
+     */
+    public List<Image> imageListAll() throws Exception;
+}
+
