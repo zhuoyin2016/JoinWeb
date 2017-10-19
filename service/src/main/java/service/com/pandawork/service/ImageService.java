6198a8cafc3e1addd7940eaf8ddbfe49931ae791
刘喆 <734576823@qq.com>
2017-10-19 18:24:47 +0800

    刘喆

---
+++ service/src/main/java/service/com/pandawork/service/ImageService.java
@@ -0,0 +1,50 @@
+package com.pandawork.service;
+
+import com.pandawork.common.entity.Image;
+import com.pandawork.common.entity.Student;
+import com.pandawork.core.common.exception.SSException;
+
+import java.util.List;
+
+/**
+ * 图片管理系统
+ * imageService层
+ * Created by houst,liuz on 2016/3/28.
+ * 增加图片，根据id删除图片，更新图片，根据id查询图片
+ *
+ */
+public interface ImageService {
+
+    /**
+     * 增加图片
+     * @throws SSException 异常
+     */
+    public void addImage(Image image) throws SSException;
+
+    /**
+     * 根据id删除图片
+     * @throws SSException 异常
+     */
+    public boolean delImageById(int id) throws SSException;
+
+    /**
+     * 更新图片
+     * @throws SSException 异常
+     */
+    public void updateImage(Image image) throws SSException;
+
+    /**
+     * 根据id查询图片
+     * @param id id
+     * @return 返回
+     * @throws Exception 异常
+     */
+    public Student queryImageById(int id) throws SSException;
+
+    /**
+     *查询图片列表
+     * @return 返回
+     * @throws SSException 异常
+     */
+    public List<Image> listAll() throws SSException;
+}
