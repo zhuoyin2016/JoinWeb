b45bbab4e8e0f4a0673c588506d6600639064ee2
zhangxiao625 <610962493@qq.com>
2017-11-08 21:26:04 +0800

    所以本菜鸡的图片显示不了 也不会套页

--- web/src/main/java/com/pandawork/web/controller/ActivityController.java
+++ web/src/main/java/com/pandawork/web/controller/ActivityController.java
@@ -4,6 +4,7 @@ import com.pandawork.common.entity.Manager;
 import com.pandawork.core.common.exception.SSException;
 import com.pandawork.core.common.log.LogClerk;
 import com.pandawork.core.common.util.Assert;
+import com.pandawork.service.ActivityService;
 import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,6 +31,7 @@ import java.util.UUID;
  */
 @Controller
 @RequestMapping("/activity")
+@SessionAttributes("activity")
 public class ActivityController extends AbstractController{
 
     /**
@@ -91,27 +93,49 @@ public class ActivityController extends AbstractController{
     }
 
     /**
-     * 增加活动待完成
+     * 增加活动
      */
     @RequestMapping(value ="/addActivity", method = RequestMethod.POST)
-    public String newActivity(Activity activity, @RequestParam("file")CommonsMultipartFile file,RedirectAttributes redirectAttributes,HttpServletRequest request) {
-        String  filename = file.getOriginalFilename();
-        String newFileName =  UUID.randomUUID()+ filename;
+    public String addActivity(Activity activity, @RequestParam("file") CommonsMultipartFile file,HttpServletRequest request) throws SSException {
+        //使用springMVC提供的CommonsMultipartFile类进行读取文件
+        String filename = file.getOriginalFilename();
+        //UUID.randomUUID()局唯一标识符,是指在一台机器上生成的数字，它保证对在同一时空中的所有机器都是唯一的
+        String newFileName = UUID.randomUUID() + filename;
+        //获取的的tomcat的路径，部署项目后相当于项目的路径
         ServletContext sc = request.getSession().getServletContext();
-        String path = sc.getRealPath("image/activityImage")+"/";
+        String path = sc.getRealPath("image/activity_img") + "/";
         File f = new File(path);
-        if(!f.exists()){
+        if (!f.exists()) {
+            //如果该文件夹不存在，则创建该文件夹
             f.mkdirs();
         }
-        try {
-            activityService.addActivity(activity);
-            redirectAttributes.addFlashAttribute("message", "添加成功！");
-            return "redirect:/activity/listAll";
-        } catch (SSException e){
-            LogClerk.errLog.error(e);
-            sendErrMsg(e.getMessage());
-            return ADMIN_SYS_ERR_PAGE;
+        if (!file.isEmpty()) {
+            try {
+                //创建一个向具有指定名称的文件中写入数据的输出文件流。
+                FileOutputStream fos = new FileOutputStream(path + newFileName);
+                //截取
+                InputStream in = file.getInputStream();
+                int b = 0;
+                while ((b = in.read()) != -1) {
+                    //写入
+                    fos.write(b);
+                }
+                //关闭流
+                fos.close();
+                in.close();
+            } catch (IOException e) {
+                LogClerk.errLog.error(e);
+                sendErrMsg(e.getMessage());
+                return ADMIN_SYS_ERR_PAGE;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return ADMIN_SYS_ERR_PAGE;
+            }
+
         }
+        activity.setActivityImg1(newFileName);
+        activityService.addActivity(activity);
+        return "redirect:/activity/listAll";
     }
 
     /**
@@ -134,6 +158,60 @@ public class ActivityController extends AbstractController{
             activityService.delActivityById(id);
             return "redirect:/activity/listAll";
         } catch (SSException e) {
+            LogClerk.errLog.error(e);
+            sendErrMsg(e.getMessage());
+            return ADMIN_SYS_ERR_PAGE;
+        }
+    }
+
+    //转到新活动
+    @RequestMapping(value = "/toUpdate/{id}", method = RequestMethod.GET)
+    public String update(@PathVariable("id") int id, Model model, HttpServletRequest request) {
+        try {
+            String message = request.getParameter("message");
+            Activity activity = activityService.queryActivityById(id);
+            model.addAttribute("activity",activity);
+            return "activity/update";         //jsp
+        } catch (Exception e) {
+            LogClerk.errLog.error(e);
+            sendErrMsg(e.getMessage());
+            return ADMIN_SYS_ERR_PAGE;
+        }
+    }
+
+    /**
+     * 修改活动
+     */
+    @RequestMapping(value = "/updateActivity", method = RequestMethod.POST)
+    public String updateActivity(Activity activity, @RequestParam("file") CommonsMultipartFile file,HttpServletRequest request) throws SSException {
+        try {
+            String filename = file.getOriginalFilename();
+            if (Assert.isNull(filename)) {
+                activityService.updateActivity(activity);
+                return "redirect:/activity/listAll";
+            } else {
+                String newFileName = UUID.randomUUID() + filename;
+                ServletContext sc = request.getSession().getServletContext();
+                String path = sc.getRealPath("image/activity_img") + "/";
+                File f = new File(path);
+                if (!f.exists()) {
+                    f.mkdirs();
+                }
+                FileOutputStream fos = new FileOutputStream(path + newFileName);
+                InputStream in = file.getInputStream();
+                int b = 0;
+                while ((b = in.read()) != -1) {
+                    fos.write(b);
+                }
+                fos.close();
+                in.close();
+                activity.setActivityImg1(newFileName);
+                activityService.updateActivity(activity);
+                return "redirect:/activity/listAll";
+            }
+
+
+        } catch (Exception e) {
             LogClerk.errLog.error(e);
             sendErrMsg(e.getMessage());
             return ADMIN_SYS_ERR_PAGE;
